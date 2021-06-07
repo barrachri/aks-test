e88d33cd1ea02d102e886222af9f532487fc7194
Christian Barra <me@christianbarra.com>
2021-06-07 22:26:56 +0100

    Try to fix error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.60.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=1.5.0"
+    }
   }
   backend "azurerm" {
     resource_group_name  = "infrastructure"
@@ -18,6 +22,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azuread" {
+  features {}
+}
+
+
 
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.ml_cluster.kube_config.0.host
